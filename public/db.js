4daa909db01764c63a879358c0f3f2cb2eeb0c61
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-08-25 20:11:44 -0500

    added all code for db.js file including an event listener to listen for the app coming back online

--- public/db.js
+++ public/db.js
@@ -31,3 +31,39 @@ function saveRecord(record) {
     // add record to your store with add method
     store.add(record);
 }
+
+function checkDatabase() {
+    // open a transaction on your pending db
+    const transaction = db.transaction(["pending"], "readwrite");
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+    // get all records from store and set to a variable
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                    method: "POST",
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                        Accept: "application/json, text/plain, */*",
+                        "Content-Type": "application/json"
+                    }
+                })
+                .then(response => response.json())
+                .then(() => {
+                    // if successful open a transaction on your pending db
+                    const transation = db.transaction(["pending"], "readwrite");
+
+                    // access your pending object store
+                    const store = transaction.objectStore("pending");
+
+                    // clear all items in your store
+                    store.clear();
+                });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
