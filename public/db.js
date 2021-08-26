5c05cd9d1d48557619b598b369d4b548409e9efd
Sarah Siegel <sarah.ac.siegel@gmail.com>
2021-08-25 20:05:57 -0500

    added a save record function to create a transaction, pending object store and add record to store within the db.js file

--- public/db.js
+++ public/db.js
@@ -16,3 +16,18 @@ request.onsuccess = function(event) {
         checkDatabase();
     }
 };
+
+request.onerror = function(event) {
+    console.log("Whoops" + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    // create a transaction on the pending db with readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    // access your pending object store
+    const store = transaction.objectStore("pending");
+
+    // add record to your store with add method
+    store.add(record);
+}
